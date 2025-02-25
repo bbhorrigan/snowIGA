86227b148b3532503ce4138e80b987107baad8b1
Peter <phorrigan@gmail.com>
2025-02-25 16:37:27 -0500

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,16 @@
 
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.76.0"
+    }
+  }
+}
+
+provider "snowflake" {
+  account  = var.snowflake_account
+  user     = var.snowflake_user
+  password = var.snowflake_password
+  role     = "ACCOUNTADMIN"
+}
