a1548f42c9343633d9e5246af6532e435c541058
Peter <phorrigan@gmail.com>
2025-02-26 14:23:11 -0500

    Delete terraform/roles directory

--- terraform/roles/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "role_name" {
-  description = "Role name to create"
-  type        = string
-}
-
-variable "assigned_users" {
-  description = "List of users to assign to this role"
-  type        = list(string)
-}
