1169942cf58bad4d559525d5516681f0009a43fa
Peter <phorrigan@gmail.com>
2025-02-25 22:19:56 -0500

    Delete terraforrom directory

--- terraforrom/roles/roles.tf
+++
@@ -1,9 +0,0 @@
-resource "snowflake_role" "iga_admin" {
-  name    = var.role_name
-  comment = "IGA Role for managing user provisioning"
-}
-
-resource "snowflake_role_grants" "iga_admin_grants" {
-  role_name = snowflake_role.iga_admin.name
-  users     = var.assigned_users
-}
