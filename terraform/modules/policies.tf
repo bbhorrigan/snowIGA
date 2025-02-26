2f8a5a9dd865d8b56fd81fb02464f73c5571a395
Peter <phorrigan@gmail.com>
2025-02-26 14:24:06 -0500

    Delete terraform/modules/policies.tf

--- terraform/modules/policies.tf
+++
@@ -1,5 +0,0 @@
-resource "snowflake_network_policy" "restricted_policy" {
-  name    = "RestrictedAccessPolicy"
-  allowed_ip_list = var.allowed_ip_list
-  comment = "Restricts access to corporate IPs"
-}
