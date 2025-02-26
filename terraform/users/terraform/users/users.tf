7adc740e31a02e756534c3c280585e3dae72d627
Peter <phorrigan@gmail.com>
2025-02-26 14:23:21 -0500

    Delete terraform/users directory

--- terraform/users/terraform/users/users.tf
+++
@@ -1,8 +0,0 @@
-resource "snowflake_user" "users" {
-  for_each    = toset(var.user_list)
-  name        = each.value
-  login_name  = each.value
-  comment     = "Provisioned via Terraform"
-  default_role = var.default_role
-  disabled    = false
-}
--- terraform/users/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "user_list" {
-  description = "List of users to be provisioned"
-  type        = list(string)
-}
-
-variable "default_role" {
-  description = "Default role for new users"
-  type        = string
-}
