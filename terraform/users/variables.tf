352ea5386461d2f7d77d930a5d959f95a483875d
Peter <phorrigan@gmail.com>
2025-02-25 22:40:38 -0500

    Create variables.tf

---
+++ terraform/users/variables.tf
@@ -0,0 +1,9 @@
+variable "user_list" {
+  description = "List of users to be provisioned"
+  type        = list(string)
+}
+
+variable "default_role" {
+  description = "Default role for new users"
+  type        = string
+}
